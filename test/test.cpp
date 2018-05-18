f71743bd669ef6e0032c378b790e1d39999f5603
SooutaTanaka <tanakakesouta@yahoo.co.jp>
2018-05-18 17:23:28 +0900

    desktop

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+#include <boost/python.hpp>
+#include <boost/numpy.hpp>
+#include <pyconfig.h>
+#define MAX_X 10
+
+namespace np = boost::numpy;
+
+int main(){
+	return 0;
+}
+
